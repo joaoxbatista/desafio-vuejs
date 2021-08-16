0aaf91e536d0a877d7a2805cdc1aa13a1d7c54e7
João Batista Gomes Silva <jhonxbatista@gmail.com>
2021-08-15 23:41:03 -0300

    ✨ Install moment and create filter to format dates

---
+++ src/utils/filters.js
@@ -0,0 +1,8 @@
+import moment from 'moment';
+import Vue from 'vue';
+
+Vue.filter('formatDate', function (value) {
+  if (value) {
+    return moment(String(value)).format('MM/DD/YYYY [-] hh[h] mm');
+  }
+});
