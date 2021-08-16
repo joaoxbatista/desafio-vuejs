9ac5d2b7b84f5912e72fde581bbba5022a737ec9
João Batista Gomes Silva <jhonxbatista@gmail.com>
2021-08-16 09:56:45 -0300

    ✏️ Fix name component

--- src/components/StatementCard/StatementCard.vue
+++ src/components/StatementCard/StatementCard.vue
@@ -51,7 +51,7 @@
   import StatementFileList from '@/components/StatementFileList/StatementFileList.vue';
 
   export default {
-    name: 'CardComunication',
+    name: 'StatementCard',
     components: {
       Card,
       StatementFileList,
